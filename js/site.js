635c7b6b538e3b47be6225973a7cfda0d91b5ed6
Ans Bilal <ansbilalabc@gmail.com>
2020-06-06 01:25:15 +0500

    8 terms and condition completed

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 let bar = document.querySelector("#loading-bar");
 let progress = document.querySelector("#progress");
-let reporter = document.querySelector("p > span");
+// let reporter = document.querySelector("p > span");
 
 let processingTime = 800;
 let i = 0;
@@ -12,15 +12,15 @@ setInterval(function() {
         processingTime = Math.floor(Math.random() * (3000 - 800));
         if (i >= 99) {
             i = 99;
-            reporter.textContent = i;
+            // reporter.textContent = i;
             bar.classList.add('complete');
             processingTime = 1000;
         }
-        reporter.textContent = i;
+        // reporter.textContent = i;
     } else {
         i = 0;
-        progress.style.width = i + "%";
-        reporter.textContent = i;
+        // progress.style.width = i + "%";
+        // reporter.textContent = i;
         bar.classList.remove('complete');
         processingTime = Math.floor(Math.random() * (3000 - 800));
     };
